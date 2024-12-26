82ba038877e0d2d99ea6322e96143e76e40d25bd
Tuhin <dastuhin.rnj@gmail.com>
2024-12-27 00:36:49 +0530

    Navbar & Herosection

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 
 function Hero() {
   return (
-    <div className='min-h-screen relative -z-10 hero bg-co grayscale flex flex-col pl-8 justify-center text-gray-300'>
-        <h1 className='text-[8rem] font-semibold w-1/ text-wrap'>
-            Lorem ipsum
+    <div className='min-h-screen relative -z-10 hero bg-co  flex flex-col px-8 justify-center'>
+        <h1 className='text-[calc(10px+15vmin)] font-semibold text-wrap leading-none md:leading-normal bg-gradient-to-br from-violet-700 to-sky-400 bg-clip-text text-transparent'>
+            Your Finacial Digest
         </h1>
-        <p className='w-1/2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio aperiam quam omnis voluptates. A sit expedita ipsam mollitia, similique eum. Quas labore, culpa aliquid assumenda, fuga illum dolorum rerum saepe, ea aperiam necessitatibus. Accusamus id tenetur eius earum! Laudantium numquam impedit, veritatis commodi provident, corporis porro et ullam, iusto deserunt eveniet repellendus laborum quaerat adipisci beatae quia! Beatae, sapiente cumque!</p>
+        <p className='text-sm md:w-1/2 md:text-2xl mt-8 rounded-xl text-gray-400'>Lorem ipsum dolor sit amet. Quas labore, culpa aliquid assumenda, fuga illum dolorum rerum saepe, ea aperiam necessitatibus. Accusamus id tenetur eius earum! Laudantium numquam impedit, veritatis commodi provident, corporis porro et ullam, iusto deserunt eveniet repellendus laborum quaerat adipisci beatae quia! Beatae, sapiente cumque!</p>
         
     </div>
   )
--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -9,25 +9,32 @@ const Navbar = () => {
     { label: "About", path: "/about" },
     { label: "Services", path: "/services" },
     { label: "Contact", path: "/contact" },
+    { label: "Blogs", path: "/blog" },
   ];
 
   // Get the current route
   const location = useLocation();
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
-      <nav className=" p-4 absolute w-screen z-1 flex justify-between">
+      <nav className=" p-4 absolute w-screen z-1 flex justify-between items-center">
 
-        <img src={Navlogo} alt="Logo" className="object-cover object-center h-10 rounded-full" />
+        <img src={Navlogo} alt="Logo" className="object-cover object-center h-14 rounded-full" />
 
-        <ul className="flex space-x-6">
+        <ul className="hidden md:flex space-x-6">
           {navItems.map((item, index) => (
             <li key={index}>
               <Link
                 to={item.path}
                 className={`text-white px-3 py-2 transition ${location.pathname === item.path
-                    ? "border-b-4 border-violet-500 font-bold"
-                    : "hover:border-b-4 border-white"
+                  ? "border-b-4 border-violet-500 font-bold"
+                  : "hover:border-b-4 border-white"
                   }`}
               >
                 {item.label}
@@ -35,6 +42,76 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+
+        {/* For mobile view */}
+
+
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="text-gray-300">
+            <svg
+              className="h-8 w-8"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {isOpen ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
+        {/* Mobile Menu */}
+        {isOpen && (
+          <div className="md:hidden text-white w-screen absolute z-1 justify-center gap-10 text-xl py-14 h-screen bg-transparent backdrop-brightness-50 backdrop-blur-xl top-0 left-0 flex flex-col items-center">
+              <img src={Navlogo} alt="Logo" className="object-cover object-center absolute top-0 left-0 m-4 h-14 rounded-full" />
+              <button onClick={toggleMenu} className="absolute top-0 right-0 m-4">
+                <svg
+                  className="h-8 w-8"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  {isOpen ? (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  ) : (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  )}
+                </svg>
+              </button>
+            
+            {navItems.map((item, index) => (
+              <Link key={index} to={item.path} className="border rounded-full w-4/5 p-2 text-center border-white/20 shadow-2xl shadow-violet-800">
+                {item.label}
+              </Link>
+            ))}
+  
+          </div>
+        )}
+
       </nav>
     </div>
   );
